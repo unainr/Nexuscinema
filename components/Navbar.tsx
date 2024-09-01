c7121e988bf6b14c04f9323d274c41d57d6b0fa1
UNAINR <munain605@gmail.com>
2024-09-02 02:41:35 +0500

    g,som

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -95,27 +95,23 @@ const Navbar = () => {
 			{isMobileMenuOpen && (
 				<div className="sm:hidden" id="mobile-menu">
 					<div className="space-y-1 px-2 pb-3 pt-2">
-						<a
-							href="#"
+						<Link
+							href="/"
 							className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
 							aria-current="page">
-							Dashboard
-						</a>
+							Home
+						</Link>
 						<Link
 							href="/movies"
 							className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
 							Movies
 						</Link>
-						<a
-							href="#"
+						<Link
+							href="/"
 							className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-							Projects
-						</a>
-						<a
-							href="#"
-							className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-							Calendar
-						</a>
+							Blog
+						</Link>
+						
             
 					</div>
 				</div>
